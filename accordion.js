1e111eb52baeccb0eb4ee7ae5a95094ce3f1ae3f
albinabist1993@mail.ru <b.hapilin@gateline.ru>
2023-02-23 15:13:32 +0500

    правки

--- accordion.js
+++ accordion.js
@@ -1,10 +1,12 @@
-var acc = document.getElementsByClassName("faq__questions-button");
-var i;
+const acc = document.getElementsByClassName("faq__questions-button");
+let i;
 
 for (i = 0; i < acc.length; i++) {
     acc[i].addEventListener("click", function() {
         this.classList.toggle("active");
-        var panel = this.nextElementSibling;
+        // this.querySelectorAll(img)[0];
+        this.querySelectorAll("img")[0].classList.toggle("faq__questions-button-close");
+        const panel = this.nextElementSibling;
         if (panel.style.display === "block") {
             panel.style.display = "none";
         } else {
